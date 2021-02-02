d298b5c112d0de4272f4ff81eeaa91c8bca83080
achizhkov <77821224+achizhkov@users.noreply.github.com>
2021-02-02 11:35:06 -0500

    Created Log in Page Test
    Created Log in Page Test

---
+++ src/test/java/com/herokuapp/theinternet/PositiveTests.java
@@ -0,0 +1,41 @@
+package com.herokuapp.theinternet;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class PositiveTests {
+
+  @Test
+  public void loginTest() {
+    System.out.println("Starting the test");
+    // Create driver
+    System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+    WebDriver driver = new ChromeDriver();
+    // Open Test Page
+    String url = "https://the-internet.herokuapp.com/";
+    driver.get(url);
+    System.out.println("The Page Is Opened");
+    //Slow Down Test
+    sleep(3000);
+    // Maximize Browser Window
+    driver.manage().window().maximize();
+    // Enter Username
+    // Enter password
+    // Click login button
+    // verifications:
+      //new url
+      //logout button is visible
+      //sucessfull login message
+  }
+
+
+    //Slow down method
+  private void sleep(long m) {
+    try {
+      Thread.sleep(m);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
